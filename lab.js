392f81aacc4ef3fefa4b67611a81e80ca239c40b
Jerso Sando <jerso.sando@gmail.com>
2021-12-15 22:42:15 -0700

    problem eight works

--- lab.js
+++ lab.js
@@ -122,7 +122,10 @@ let passingScore = 7
 */
 
 //CODE HERE
-
+while (score < passingScore){
+  console.log("Your score is not high enough")
+  score += 1
+};
 ////////// INTERMEDIATE PROBLEMS //////////
 
 ////////// PROBLEM 9 //////////
